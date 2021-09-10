1b569b1782cc06ccfb530e94500e32179f52751c
Mircea Călin Rusu <mircea.rusu@drd.unibuc.ro>
2021-09-10 18:47:00 +0300

    first commit: we talked about how we create packages in Python

---
+++ clustering/__init__.py
@@ -0,0 +1,16 @@
+def main():
+    pass  # placeholder pt cod
+
+
+# main+Tab autocompleted tot the if necessary when running this script
+if __name__ == '__main__':
+    # aici punem codul care se executa strict la rularea lui __init__.py
+    # ADICA nu se executa cand importam in main vreo functie de aici
+    main()
+
+
+# cu rosu sunt scrise fisierele noi (untracked)
+# pentru a le adauga la un commit (o actualizare a fisierelor adica a schimbarilor din proiect) al proiectului
+#   Selectam fisierele (sau folderul mare) si apasam Ctrl+Alt+A
+# sunt cu verde cand apar la tracked
+# Pentru a salva local commit-ul la care am adaugat schimbarile, este nevoie de dam Ctrl+K
